7dd55edbb1cbd554817a99f0a8ad48ef0a95fb95
Damiano Azzolini <damiano.azzolini@unife.it>
2020-11-08 23:14:02 +0100

    argument parser

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 // #include <SWI-Prolog.h>
+#include <argp.h>
 #include "symdiff.h"
 #include <stdlib.h>
 #include <sys/types.h> 
@@ -9,21 +10,91 @@
 
 #define SYMDIFF_VERSION 0.1
 #define FORK_ERROR_EXIT -2
-#define MANUAL_EXIT -1
 
-void print_manual() {
-	printf("SYMDIFF - compute symbolic derivations\n");
-	printf("usage: symdiff <function> <variable>\n");
-	printf("example: symdiff \"3*x^2\" \"x\"\n");
-	printf("example: symdiff \"3*x^2 + y\" \"[x,y]\"\n");
-	printf("options: \n");
-	printf("\t-p --print-steps: \n");
-	printf("\t-t --tex: \n");
-	printf("\t-e --evaluate: \n");
-	printf("\t-h --hessian: \n");
-	printf("\t-j --jacobian: \n");
-	printf("\t-i --interactive: \n");
+error_t argp_err_exit_status = -1;
+
+const char *argp_program_version = "symdiff 0.0.1";
+const char *argp_program_bug_address = "<github_issue>";
+const char doc[] = "SYMDIFF -- symbolic derivative and more\nexample: symdiff 3*x^2 -d x\nnexample: symdiff 3*x^2 + y -d [x,y]";
+
+const char args_doc[] = "EQUATION VARIABLE(s)";
+static struct argp_option options[] = {
+	{"verbose",  	'v', 0,      	0,	"Produce verbose output", -1 },
+	{"quiet",    	'q', 0,      	0,  "Don't produce any output", -1 },
+	{"output",   	'o', "FILE", 	0,	"Output to FILE instead of standard output", -1 },
+	// {"derivate",	'd', "VARIABLE(s)",0,	"Compute derivative w.r.t. VARIABLE(s)", 0 },
+	{"print-steps",	'p', 0, 		0,	"Print derivation steps", 1 },
+	{"tex",			't', 0, 		0,	"Print latex output", 2 },
+	{"evaluate",	'e', 0, 		0,	"Evaluate the derivative", 1 },
+	{"jacobian",	'j', 0, 		0,	"Compute Jacobian",2 },
+	{"hessian",		'H', 0, 		0,	"Compute Hessian", 2 },
+	{"interactive",	'i', 0, 		0,	"Interactive mode", 3 },
+	{ 0 }
+};
+
+struct arguments {
+	char *args[2];                /* EQUATION & VARIABLE(s) */
+	int quiet, verbose, print_steps, latex, evaluate, hessian, jacobian, interactive;
+	char *output_file;
+};
+
+error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+
+	switch (key) {
+	case 'q':
+		if(arguments->verbose == 0)
+			arguments->quiet = 1;
+		break;
+	case 'v':
+		if(arguments->quiet == 0)
+			arguments->verbose = 1;
+		break;
+	case 'o':
+		arguments->output_file = arg;
+		break;
+	case 'p':
+		arguments->print_steps = 1;
+		break;
+	case 't':
+		arguments->latex = 1;
+		break;
+	case 'e':
+		arguments->evaluate = 1;
+		break;
+	case 'j':
+		arguments->jacobian = 1;
+		break;
+	case 'H':
+		arguments->hessian = 1;
+		break;
+	case 'i':
+		arguments->interactive = 1;
+		break;
+		
+	case ARGP_KEY_ARG:
+		if (state->arg_num > 2)
+			/* Too many arguments. */
+			argp_usage(state);
+
+		arguments->args[state->arg_num] = arg;
+
+		break;
+
+	case ARGP_KEY_END:
+		if (state->arg_num != 2)
+			/* Not enough arguments. */
+			argp_usage(state);
+			// exit(MANUAL_EXIT);
+		break;
+
+	default:
+		return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
 }
+
+static struct argp argp = { options, parse_opt, args_doc, doc, NULL, NULL,NULL };
 
 int main(int argc, char **argv) { 
 	int pid;
@@ -31,11 +102,22 @@ int main(int argc, char **argv) {
 	int fd[2];
 	char command[100] = "evaluate(";
 	char readbuffer[100];
+	struct arguments arguments;
 
-	if(argc <= 2) {
-		print_manual();
-		exit(MANUAL_EXIT);
-	}
+	/* Default values. */
+	arguments.quiet = 0;
+	arguments.verbose = 0;
+	arguments.print_steps = 0;
+	arguments.latex = 0;
+	arguments.evaluate = 0;
+	arguments.hessian = 0;
+	arguments.jacobian = 0;
+	arguments.interactive = 0;
+	arguments.output_file = "-";
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	printf("args[0], args[1]: %s, %s\n",arguments.args[0],arguments.args[1]);
 
 	pipe(fd);
 
